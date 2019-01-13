077d88d045762509f45507156370ab401ba6b633
DANIELLE PARK <dpark1008@gmail.com>
2019-01-13 00:34:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,1 @@
 # Your code goes here!
-class Anagram
-  attr_accessor :words
-
-  def initialize(words)
-    @words = words
-  end
-
-  def match(array)
-    array.select do |words|
-      words.split(" ").sort == @word.split(" ").sort
-    end
-  end
-end
